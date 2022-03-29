60729482bdc027796e335b554a979531be7e8b0e
araupontones <43657130+araupontones@users.noreply.github.com>
2022-03-29 12:05:14 +0200

    readme

---
+++ R/Delivery/create_delivery_data.R
@@ -0,0 +1,88 @@
+#paths define in .Rprofile
+#' Input: clean data from the survey field work
+#' Output: delivery data (only approved files)
+#' data is imported from dir_fp_clean
+#' The imported data is created in R/Follow-up/QA/3.clean_and_exort_data_dashboard.R
+#' exported to dir_delivery
+
+
+
+
+#i. confirm that there are no duplicates ---------------------------------------
+main <- import(file.path(dir_fp_clean, "grandfollowup2022.dta"))
+
+duplicates <- main %>% get_dupes(ID_participant) %>% select(ID_participant, outcome, status)
+
+
+
+#ii. Remove the management variables and relocate variables to improve browsing
+main_clean <- main %>%
+  select(-c(url, last_action,time, link, date,entities__errors,
+            questions__comments,rejections__hq, rejections__sup,
+            sssys_irnd, concatPart, concatProj, interviewers,
+            final_attemps, interview__id, interview__status,
+            has__errors, n_questions_unanswered, interview__duration,
+            inquiridor, responsible
+            )) %>%
+  mutate(outcome = susor::susor_get_stata_labels(outcome)) %>%
+  relocate(interview__key, ID_participant, participante, project,status, Management) %>%
+  rename(Field_Management = Management,
+         Status_field = status) %>%
+  filter(Field_Management == "APPROVED")
+
+
+
+
+#iii. drop redundant interviews in rosters and save-------------------------------------
+
+
+#list of clean files
+clean_files <- list.files(dir_fp_clean, full.names = T)
+#identify main file
+main_file <- which(str_detect(clean_files, "grandfollowup"))
+#roster files
+roster_files <- clean_files[-main_file]
+
+#main files with ids to share with the rosters
+main_ids <- main_clean %>% select(interview__key, ID_participant)
+
+
+#Fetch ID_participant to each roster, drop redundant observations, and save
+
+clean_rosters <- lapply(roster_files, function(roster){
+  
+  #get name of stata file
+  stata_file <- str_extract(roster, "[^\\/]+$")
+  message(stata_file)
+  
+  #import clean roster
+  clean_roster <- import(roster)
+  print(paste("rows before join:",nrow(clean_roster)))
+  
+  #get IDs from main questionnaire and drop redundant interviews
+  # drop those that were removed from the main file
+  roster_ids <- clean_roster %>% left_join(main_ids, by = "interview__key") %>%
+    filter(!is.na(ID_participant))
+  
+  
+  print(paste("rows after join:",nrow(roster_ids)))
+  
+  #save to delivery
+  
+  rio::export(roster_ids, file.path(dir_delivery, stata_file))
+  
+  
+})
+
+
+#iv. Save main file -----------------------------------------------------------
+
+rio::export(main_clean, file.path(dir_delivery, "grandfollowup2022.dta"))
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# MUVA-fwup
-Design, deployment and analysis of the MUVA follow up study
+# Design, deployment and analysis of the MUVA follow up study
+
+Below is a description of the directories, their input, outputs and objectives. For each task, there is an R file with the code and a /data one with its data.
+
+All the data paths are defined in .Rprofile
+
+## Reference
+
+It creates external reference data for the survey. Specifically, it gets the clean name of the provinces, cidades, and bairros of Mozambique. 
+
+Output: data/Reference/cidades.xlsx
+
+## Preparation
+
+It imports the data of participants for each project that the team was able to collect from the ligada historical dropbox. 
+
+R/Preparation/raw imports all the data from dropbox and appends into a single file.
+R/Preparation/copy_raw_from_dropbox 
+
+R/Preparation/clean it is a series of scripts that cleans the sample frame (homogenizes the variables accross projects)  
+
+
+Outputs: (dir_prep_clean, "muva_follow_up_clean.xlsx"), this data was the main input for the zoho creator app to confirm the contact details of the participants.
+
+
+## Confirmation
+
+This process was implemented to contact all the participants from (dir_prep_clean/"muva_follow_up_clean.xlsx") and confirm their contact details. The output of this excersice was used as the sample for the follow up survey.
+
+
+Outputs: dir_conf_clean/"confirmation_clean.rds" which was used as the sample for the follow up survey. Only participants for which the contact details were confirmed.
+
+
+## Follow-UP
+
+It imports the output from the confirmation step, crates lookup tables for survey solution, and executes the QA system of the field work. 
+
+Output: Data for dashboard
+Output2: Clean data of the survey "data/Follow-up/2.clean". This data that includes all the field management variables is the input to create the delivery data
+
+#Delivery
+Takes the clean data from the survey, keeps only approved cases, and removes management variables
+
+Output: dir_delivery and dropbox/data/data_survey
+
+
+
+
